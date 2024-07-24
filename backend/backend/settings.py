f0493fcd79911a9e6987eaaa9650ad4b6fe4d855
Hikmatullah <hikmathakim7200@gmail.com>
2024-07-24 21:17:03 +0100

    Preparing dependencies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,19 +11,32 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uy&zrtteh57zg$qv1ku=fty6nyayibfr3qvw6d3t%)*k=9r9_b'
+# SECRET_KEY = 'django-insecure-uy&zrtteh57zg$qv1ku=fty6nyayibfr3qvw6d3t%)*k=9r9_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
